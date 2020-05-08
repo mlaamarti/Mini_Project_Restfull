c57fb0d4107bd205f808ce25d36f933c32ee18f3
hamza-gh <hamza.ghiwach.97@gmail.com>
2020-05-08 13:20:56 +0000

    new one

---
+++ .Net/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.Ajouter = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1_aj = new System.Windows.Forms.PictureBox();
+            this.Supprimer = new System.Windows.Forms.Button();
+            this.Modifier = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Afficher = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.id_mod = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1_aj)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(33, 225);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(545, 323);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(596, 248);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(54, 40);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 84;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Ajouter
+            // 
+            this.Ajouter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.Ajouter.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Ajouter.CausesValidation = false;
+            this.Ajouter.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Ajouter.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Navy;
+            this.Ajouter.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ajouter.ForeColor = System.Drawing.Color.Black;
+            this.Ajouter.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Ajouter.Location = new System.Drawing.Point(593, 239);
+            this.Ajouter.Name = "Ajouter";
+            this.Ajouter.Size = new System.Drawing.Size(220, 59);
+            this.Ajouter.TabIndex = 83;
+            this.Ajouter.Text = "     Create";
+            this.Ajouter.UseVisualStyleBackColor = false;
+            this.Ajouter.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(595, 327);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(54, 40);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 82;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1_aj
+            // 
+            this.pictureBox1_aj.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1_aj.Image")));
+            this.pictureBox1_aj.Location = new System.Drawing.Point(606, 404);
+            this.pictureBox1_aj.Name = "pictureBox1_aj";
+            this.pictureBox1_aj.Size = new System.Drawing.Size(39, 42);
+            this.pictureBox1_aj.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1_aj.TabIndex = 167;
+            this.pictureBox1_aj.TabStop = false;
+            // 
+            // Supprimer
+            // 
+            this.Supprimer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.Supprimer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Supprimer.CausesValidation = false;
+            this.Supprimer.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Supprimer.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Navy;
+            this.Supprimer.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Supprimer.ForeColor = System.Drawing.Color.Black;
+            this.Supprimer.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Supprimer.Location = new System.Drawing.Point(592, 318);
+            this.Supprimer.Name = "Supprimer";
+            this.Supprimer.Size = new System.Drawing.Size(221, 59);
+            this.Supprimer.TabIndex = 81;
+            this.Supprimer.Text = "      Delete";
+            this.Supprimer.UseVisualStyleBackColor = false;
+            this.Supprimer.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Modifier
+            // 
+            this.Modifier.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.Modifier.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Modifier.CausesValidation = false;
+            this.Modifier.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Modifier.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Navy;
+            this.Modifier.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Modifier.ForeColor = System.Drawing.Color.Black;
+            this.Modifier.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Modifier.Location = new System.Drawing.Point(595, 398);
+            this.Modifier.Name = "Modifier";
+            this.Modifier.Size = new System.Drawing.Size(218, 59);
+            this.Modifier.TabIndex = 166;
+            this.Modifier.Text = "    Update";
+            this.Modifier.UseVisualStyleBackColor = false;
+            this.Modifier.Click += new System.EventHandler(this.button1_aj_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(604, 477);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 169;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Afficher
+            // 
+            this.Afficher.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.Afficher.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Afficher.CausesValidation = false;
+            this.Afficher.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Afficher.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Navy;
+            this.Afficher.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Afficher.ForeColor = System.Drawing.Color.Black;
+            this.Afficher.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Afficher.Location = new System.Drawing.Point(595, 472);
+            this.Afficher.Name = "Afficher";
+            this.Afficher.Size = new System.Drawing.Size(218, 59);
+            this.Afficher.TabIndex = 168;
+            this.Afficher.Text = "      Show ALL";
+            this.Afficher.UseVisualStyleBackColor = false;
+            this.Afficher.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(102, 45);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 25);
+            this.label8.TabIndex = 170;
+            this.label8.Text = "Id  :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(71, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 25);
+            this.label1.TabIndex = 171;
+            this.label1.Text = "Number  :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(56, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 25);
+            this.label2.TabIndex = 172;
+            this.label2.Text = "ExpiryDate  :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(438, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(203, 25);
+            this.label3.TabIndex = 173;
+            this.label3.Text = "ContoleNumber  :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(499, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 25);
+            this.label4.TabIndex = 174;
+            this.label4.Text = "Type  :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(211, 105);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(156, 28);
+            this.textBox2.TabIndex = 176;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(211, 174);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(156, 28);
+            this.textBox3.TabIndex = 177;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(647, 33);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(166, 28);
+            this.textBox4.TabIndex = 178;
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // id_mod
+            // 
+            this.id_mod.AutoSize = true;
+            this.id_mod.Location = new System.Drawing.Point(218, 54);
+            this.id_mod.Name = "id_mod";
+            this.id_mod.Size = new System.Drawing.Size(0, 13);
+            this.id_mod.TabIndex = 180;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Visa",
+            "MasterCard",
+            "Paypal"});
+            this.comboBox1.Location = new System.Drawing.Point(647, 112);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(166, 21);
+            this.comboBox1.TabIndex = 181;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 551);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.id_mod);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Afficher);
+            this.Controls.Add(this.pictureBox1_aj);
+            this.Controls.Add(this.Modifier);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.Ajouter);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.Supprimer);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " CreditCard";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1_aj)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button Ajouter;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        public System.Windows.Forms.PictureBox pictureBox1_aj;
+        private System.Windows.Forms.Button Supprimer;
+        public System.Windows.Forms.Button Modifier;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.Button Afficher;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label id_mod;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ .Net/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string url = "http://localhost:8080/api/v1/creditcard/";
+
+  
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            id_mod.Text = "None";
+            Show_ALL();
+
+
+        }
+        void Show_ALL()
+        {
+            var webRequest = (HttpWebRequest)WebRequest.Create(url);
+            var webResponse = (HttpWebResponse)webRequest.GetResponse();
+
+            var reader = new StreamReader(webResponse.GetResponseStream());
+            string s = reader.ReadToEnd();
+            var arr = JsonConvert.DeserializeObject<JArray>(s);
+            dataGridView1.DataSource = arr;
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Show ALL
+
+            Show_ALL();
+            
+        }
+
+
+
+
+        private void button1_aj_Click(object sender, EventArgs e)
+        {
+            string message = "Are you sure to Edit this client !!";
+            string title = "Modification";
+
+            //Modifier => PUT
+            try
+            {
+             
+                    var httpWebRequest = (HttpWebRequest)WebRequest.Create(url + id_mod.Text);
+                    httpWebRequest.ContentType = "application/json";
+                    httpWebRequest.Method = "PUT";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+             { 
+              string json = " {\r\n      \r\n        \"number\": \"" + textBox2.Text + "\",\r\n        \"expiryDate\": \"" + textBox3.Text + "\",\r\n        \"controlNumber\": " + textBox4.Text + ",\r\n        \"type\": \"" + comboBox1.Text + "\"\r\n    }";
+                        streamWriter.Write(json);
+
+
+             }
+
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+               
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult r = MessageBox.Show(message, title, buttons);
+                if (r == DialogResult.Yes)
+                {
+                    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                    {
+                        var result = streamReader.ReadToEnd();
+                    }
+                    Show_ALL();
+                    Emptyfield();
+                    MessageBox.Show("Customer is Correctly Modify");
+                }
+                else
+                    MessageBox.Show("Operation failed");
+            }
+            catch 
+            {
+                MessageBox.Show("Double click on the ROW that you want to modify on the DataGridView");
+            }
+        }
+        
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //ADD with POST
+            try
+            {
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    string json = " {\r\n      \r\n        \"number\": \"" + textBox2.Text + "\",\r\n        \"expiryDate\": \"" + textBox3.Text + "\",\r\n        \"controlNumber\": " + textBox4.Text + ",\r\n        \"type\": \"" + comboBox1.Text + "\"\r\n    }";
+                    streamWriter.Write(json);
+
+                }
+
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                }
+
+                Show_ALL();
+            }
+            catch
+            {
+                MessageBox.Show("Check if all fields is full");
+            }
+            
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string message = "Are you sure !!";
+            string title = "Deleting ";
+            // Delete 
+            try
+            {
+
+                
+              
+                    var httpWebRequest = (HttpWebRequest)WebRequest.Create(url + id_mod.Text);
+
+                    httpWebRequest.Method = "DELETE";
+
+
+
+
+                    var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult r = MessageBox.Show(message, title, buttons);
+                if (r == DialogResult.Yes)
+                {
+                    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                    {
+                        var result = streamReader.ReadToEnd();
+                    }
+                    Show_ALL();
+                    Emptyfield();
+                }
+                else
+                    MessageBox.Show("Operation failed");
+            }
+            catch
+            {
+                MessageBox.Show("Double Click on The Column where the customer is located then click on the Delete button");
+            }
+            
+           
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            
+            id_mod.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textBox4.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            comboBox1.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+        }
+        void Emptyfield()
+        {
+            id_mod.Text = "None";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            comboBox1.Text = "";
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+                if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                    (e.KeyChar != '.'))
+                {
+                    e.Handled = true;
+                }
+
+                // only allow one decimal point
+                if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+                {
+                    e.Handled = true;
+                }
+            
+        }
+    }
+   
+}
